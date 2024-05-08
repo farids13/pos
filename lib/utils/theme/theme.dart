d95d9b04642e039774b3f827fd5d0c0ba26a87a4
Farid Satria <faridsatria24@gmail.com>
2024-05-08 17:54:24 +0700

    Implement Theme Custom

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,4 +1,10 @@
+import 'package:cashier_app/utils/theme/custom_theme/appbar_theme.dart';
+import 'package:cashier_app/utils/theme/custom_theme/bottom_sheet_theme.dart';
+import 'package:cashier_app/utils/theme/custom_theme/checkbox_theme.dart';
+import 'package:cashier_app/utils/theme/custom_theme/chip_theme.dart';
 import 'package:cashier_app/utils/theme/custom_theme/elevated_button_theme.dart';
+import 'package:cashier_app/utils/theme/custom_theme/outlined_button_theme.dart';
+import 'package:cashier_app/utils/theme/custom_theme/text_field_theme.dart';
 import 'package:cashier_app/utils/theme/custom_theme/text_theme.dart';
 import 'package:flutter/material.dart';
 
@@ -7,31 +13,33 @@ class QAppTheme {
 
   static ThemeData lightTheme = ThemeData(
     useMaterial3: true,
-    colorScheme: ColorScheme.fromSeed(
-      seedColor: Colors.blue,
-      brightness: Brightness.light,
-    ),
-    scaffoldBackgroundColor: Colors.white,
-    appBarTheme: const AppBarTheme(
-      foregroundColor: Colors.black,
-      backgroundColor: Colors.white,
-    ),
+    fontFamily: 'Poppins',
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
     textTheme: QTextTheme.lightTextTheme,
+    chipTheme: QChipTheme.lightChipTheme,
+    scaffoldBackgroundColor: Colors.white,
+    appBarTheme: QAppBarTheme.lightAppBarTheme,
     elevatedButtonTheme: QElevatedButtonTheme.lightElevatedButtonTheme,
+    checkboxTheme: QCheckBoxTheme.lightCheckboxTheme,
+    bottomSheetTheme: QBottomSheetTheme.lightBottomSheetTheme,
+    outlinedButtonTheme: QOutlinedButtonTheme.lightOutlinedButtonTheme,
+    inputDecorationTheme: QTextFieldTheme.lightInputDecorationTheme,
   );
 
   static ThemeData darkTheme = ThemeData(
     useMaterial3: true,
-    colorScheme: ColorScheme.fromSeed(
-      seedColor: Colors.red,
-      brightness: Brightness.dark,
-    ),
-    scaffoldBackgroundColor: Colors.black,
-    appBarTheme: const AppBarTheme(
-      foregroundColor: Colors.white,
-      backgroundColor: Colors.black,
-    ),
+    fontFamily: 'Poppins',
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
     textTheme: QTextTheme.darkTextTheme,
+    chipTheme: QChipTheme.darkChipTheme,
+    scaffoldBackgroundColor: Colors.black,
+    appBarTheme: QAppBarTheme.darkAppBarTheme,
     elevatedButtonTheme: QElevatedButtonTheme.darkElevatedButtonTheme,
+    checkboxTheme: QCheckBoxTheme.darkCheckboxTheme,
+    bottomSheetTheme: QBottomSheetTheme.darkBottomSheetTheme,
+    outlinedButtonTheme: QOutlinedButtonTheme.darkOutlinedButtonTheme,
+    inputDecorationTheme: QTextFieldTheme.darkInputDecorationTheme,
   );
 }
